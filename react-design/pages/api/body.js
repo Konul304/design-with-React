12a96473db22b4d171d663fdd15394f94ab7d111
Konul304 <memmedovakonul270@gmail.com>
2023-02-28 17:17:28 +0400

    switch to React Paginate

--- react-design/pages/api/body.js
+++ react-design/pages/api/body.js
@@ -9,63 +9,13 @@ import axios from "axios";
 import moment from 'moment';
 moment().format();
 import "bootstrap/dist/css/bootstrap.min.css";
+import ReactPaginate from "react-paginate"
 
 export default function Body() {
 
     const [getArray, setArray] = useState([])
     const [inputValue, setInputValue] = useState('');
     const [inputValue2, setInputValue2] = useState('');
-    const [perPage, setPerPage] = useState(6);
-    const [currentPage, setCurrentPage] = useState(1);
-
-    function ShowData() {
-        const indexOfLastPage = currentPage * perPage;
-        const indexOfFirstPage = indexOfLastPage - perPage;
-        const currentPosts = getArray.slice(indexOfFirstPage, indexOfLastPage)
-        console.log(currentPosts);
-        return currentPosts.map((item, index) => {
-            return (
-                <tbody>
-                    <tr>
-                        <td className="cell-padding cell-padding-left-2">{item.id}</td>
-                        <td className="cell-padding cell-padding-left-2">{item.no}</td>
-                        <td className="cell-padding cell-padding-left">{item.createdAt}</td>
-                        <td className="cell-padding cell-padding-left-2">{item.cost}</td>
-                        <td className="cell-padding cell-padding-left-2">{item.quantity}</td>
-                        <td className="pt-4"><div className="key"><FontAwesomeIcon className="key1" icon={faKey} /></div></td>
-                        <td className="cell-padding cell-padding-left"><div className="reason">{item.reason}</div></td>
-                        <td className="cell-padding cell-padding-left pe-4"><div className="status">{item.status}</div></td>
-                    </tr>
-                </tbody>
-            )
-        })
-    }
-
-    function ShowPagination() {
-        const [pageNumbers, setPageNumbers] = useState([]);
-        const totalPosts = getArray.length;
-
-        for (let i = 1; i <= Math.ceil(totalPosts / perPage); i++) {
-            pageNumbers.push(i)
-        }
-
-        const pagination = (pageNumbers) => {
-            setCurrentPage(pageNumbers);
-        }
-
-        return (
-            <nav>
-                <ul className="pagination pt-3">
-                    {pageNumbers.map(number => (
-                        <li key={number} className={currentPage === number ? 'page-item active' : 'page-item'}>
-                            <button onClick={() => pagination(number)} className="page-link"> {number} </button>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-        )
-    }
-
     useEffect(() => {
         axios({
             method: "GET",
@@ -76,6 +26,100 @@ export default function Body() {
             })
     }, [])
 
+    const items = getArray;
+
+    function Items({ currentItems }) {
+        return (
+            <>
+                <table className="table">
+                    <thead className="cell-padding-left-2 ">
+                        <tr>
+                            <th className=" cell-padding cell-padding-left" scope="col">N.A.S NO:</th>
+                            <th className=" cell-padding cell-padding-left" scope="col">Reqress NO:</th>
+                            <th className=" cell-padding cell-padding-left" scope="col">Tarix</th>
+                            <th className=" cell-padding cell-padding-left" scope="col">Ümumi məbləğ</th>
+                            <th className=" cell-padding cell-padding-left" scope="col">Miqdar</th>
+                            <th className=" cell-padding" scope="col">E-imza</th>
+                            <th className=" cell-padding cell-padding-left-3" scope="col">Səbəb</th>
+                            <th className=" cell-padding cell-padding-left-3" scope="col">Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            currentItems.filter((val) => {
+                                return select(val);
+                            }).map((val) => (
+                                <tr>
+                                    <td className="cell-padding cell-padding-left-2">{val.id}</td>
+                                    <td className="cell-padding cell-padding-left-2">{val.no}</td>
+                                    <td className="cell-padding cell-padding-left">
+                                        {moment(val.createdAt).format('DD.MM.YY , h:mm:ss')}
+                                    </td>
+                                    <td className="cell-padding cell-padding-left-2">{val.cost}</td>
+                                    <td className="cell-padding cell-padding-left-2">{val.quantity}</td>
+                                    <td className="pt-4">
+                                        <div className="key"><FontAwesomeIcon className="key1" icon={faKey} /></div>
+                                    </td>
+                                    <td className="cell-padding cell-padding-left"><div className="reason">{val.reason}</div></td>
+                                    <td className="cell-padding cell-padding-left pe-4"><div className="status">{val.status}</div></td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+            
+                </table>
+            </>
+        );
+    }
+
+    const PaginatedItems = ({ itemsPerPage }) => {
+        // Here we use item offsets; we could also use page offsets
+        // following the API or data you're working with.
+        const [itemOffset, setItemOffset] = useState(0);
+
+        // Simulate fetching items from another resources.
+        // (This could be items from props; or items loaded in a local state
+        // from an API endpoint with useEffect and useState)
+        const endOffset = itemOffset + itemsPerPage;
+        console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+        const currentItems = items.slice(itemOffset, endOffset);
+        const pageCount = Math.ceil(items.length / itemsPerPage);
+
+        // Invoke when user click to request another page.
+        const handlePageClick = (event) => {
+            const newOffset = (event.selected * itemsPerPage) % items.length;
+            console.log(
+                `User requested page number ${event.selected}, which is offset ${newOffset}`
+            );
+            setItemOffset(newOffset);
+        };
+
+        return (
+            <>
+                <Items currentItems={currentItems} />
+                <ReactPaginate
+                    nextLabel="next >"
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={3}
+                    marginPagesDisplayed={2}
+                    pageCount={pageCount}
+                    previousLabel="< previous"
+                    pageClassName="page-item"
+                    pageLinkClassName="page-link"
+                    previousClassName="page-item"
+                    previousLinkClassName="page-link"
+                    nextClassName="page-item"
+                    nextLinkClassName="page-link"
+                    breakLabel="..."
+                    breakClassName="page-item"
+                    breakLinkClassName="page-link"
+                    containerClassName="pagination"
+                    activeClassName="active"
+                    renderOnZeroPageCount={null}
+                />
+            </>
+        );
+    }
 
     const handleChange = (e) => {
 
@@ -134,63 +178,7 @@ export default function Body() {
                 </div>
             </div>
             <div className="tableContent">
-                {/* <table className="table">
-                    <thead className="cell-padding-left-2 ">
-                        <tr>
-                            <th className=" cell-padding cell-padding-left" scope="col">N.A.S NO:</th>
-                            <th className=" cell-padding cell-padding-left" scope="col">Reqress NO:</th>
-                            <th className=" cell-padding cell-padding-left" scope="col">Tarix</th>
-                            <th className=" cell-padding cell-padding-left" scope="col">Ümumi məbləğ</th>
-                            <th className=" cell-padding cell-padding-left" scope="col">Miqdar</th>
-                            <th className=" cell-padding" scope="col">E-imza</th>
-                            <th className=" cell-padding cell-padding-left-3" scope="col">Səbəb</th>
-                            <th className=" cell-padding cell-padding-left-3" scope="col">Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            getArray.filter((val) => {
-                                return select(val);
-                            }).map((val) => (
-                                <tr>
-                                    <td className="cell-padding cell-padding-left-2">{val.id}</td>
-                                    <td className="cell-padding cell-padding-left-2">{val.no}</td>
-                                    <td className="cell-padding cell-padding-left">
-                                        {moment(val.createdAt).format('DD.MM.YY , h:mm:ss')}
-                                    </td>
-                                    <td className="cell-padding cell-padding-left-2">{val.cost}</td>
-                                    <td className="cell-padding cell-padding-left-2">{val.quantity}</td>
-                                    <td className="pt-4">
-                                        <div className="key"><FontAwesomeIcon className="key1" icon={faKey} /></div>
-                                    </td>
-                                    <td className="cell-padding cell-padding-left"><div className="reason">{val.reason}</div></td>
-                                    <td className="cell-padding cell-padding-left pe-4"><div className="status">{val.status}</div></td>
-                                </tr>
-                            ))
-                        }
-                    </tbody>
-                </table> */}
-
-                <table className="table align-items-center justify-content-center mb-0">
-                    <thead>
-                        <tr>
-                            <th className="cell-padding cell-padding-left" >N.A.S NO:</th>
-                            <th className="cell-padding cell-padding-left" >Reqress NO:</th>
-                            <th className="cell-padding cell-padding-left" >Tarix</th>
-                            <th className="cell-padding cell-padding-left" >Ümumi məbləğ</th>
-                            <th className="cell-padding cell-padding-left" >Miqdar</th>
-                            <th className="cell-padding">E-imza</th>
-                            <th className="cell-padding cell-padding-left-3">Səbəb</th>
-                            <th className="cell-padding cell-padding-left-3">Status</th>
-                        </tr>
-                    </thead>
-                    <ShowData />
-                </table>
-
-                <div style={{ float: 'right' }}>
-                    <ShowPagination />
-                </div>
-
+                <PaginatedItems itemsPerPage={6} />
             </div>
         </>
     )
