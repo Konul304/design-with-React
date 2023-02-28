7c28502ac829709b0ba813fabe044f5a9090ad48
Konul304 <memmedovakonul270@gmail.com>
2023-02-28 14:19:16 +0400

    feat: pagination

--- react-design/pages/api/body.js
+++ react-design/pages/api/body.js
@@ -9,16 +9,62 @@ import axios from "axios";
 import moment from 'moment';
 moment().format();
 import "bootstrap/dist/css/bootstrap.min.css";
-// import paginationFactory from "react-bootstrap-table2-paginator";
-// import BootstrapTable from "react-bootstrap-table-next";
-// import "react-bootstrap-table-next/dist/react-bootstrap-table2.css";
-// import "react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css";
 
 export default function Body() {
 
     const [getArray, setArray] = useState([])
     const [inputValue, setInputValue] = useState('');
-    const [inputValue2, setInputValue2] = useState('')
+    const [inputValue2, setInputValue2] = useState('');
+    const [perPage, setPerPage] = useState(6);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    function ShowData() {
+        const indexOfLastPage = currentPage * perPage;
+        const indexOfFirstPage = indexOfLastPage - perPage;
+        const currentPosts = getArray.slice(indexOfFirstPage, indexOfLastPage)
+        console.log(currentPosts);
+        return currentPosts.map((item, index) => {
+            return (
+                <tbody>
+                    <tr>
+                        <td className="cell-padding cell-padding-left-2">{item.id}</td>
+                        <td className="cell-padding cell-padding-left-2">{item.no}</td>
+                        <td className="cell-padding cell-padding-left">{item.createdAt}</td>
+                        <td className="cell-padding cell-padding-left-2">{item.cost}</td>
+                        <td className="cell-padding cell-padding-left-2">{item.quantity}</td>
+                        <td className="pt-4"><div className="key"><FontAwesomeIcon className="key1" icon={faKey} /></div></td>
+                        <td className="cell-padding cell-padding-left"><div className="reason">{item.reason}</div></td>
+                        <td className="cell-padding cell-padding-left pe-4"><div className="status">{item.status}</div></td>
+                    </tr>
+                </tbody>
+            )
+        })
+    }
+
+    function ShowPagination() {
+        const [pageNumbers, setPageNumbers] = useState([]);
+        const totalPosts = getArray.length;
+
+        for (let i = 1; i <= Math.ceil(totalPosts / perPage); i++) {
+            pageNumbers.push(i)
+        }
+
+        const pagination = (pageNumbers) => {
+            setCurrentPage(pageNumbers);
+        }
+
+        return (
+            <nav>
+                <ul className="pagination pt-3">
+                    {pageNumbers.map(number => (
+                        <li key={number} className={currentPage === number ? 'page-item active' : 'page-item'}>
+                            <button onClick={() => pagination(number)} className="page-link"> {number} </button>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        )
+    }
 
     useEffect(() => {
         axios({
@@ -30,39 +76,6 @@ export default function Body() {
             })
     }, [])
 
-    // const products = getArray;
-
-    // const columns = [
-    //   { dataField: "id", text: "Id" },
-    //   { dataField: "no", text: "Name" },
-    //   { dataField: "createdAt", text: "Animal" },
-    //   { dataField: "cost", text: "Animal" },
-    //   { dataField: "quantity", text: "Animal" },
-    //   { dataField: "reason", text: "Animal" },
-    //   { dataField: "status", text: "Animal" }
-    // ];
-  
-    // console.log(getArray);
-  
-    // const pagination = paginationFactory({
-    //   page: 2,
-    //   sizePerPage: 2,
-    //   lastPageText: ">>",
-    //   firstPageText: "<<",
-    //   nextPageText: ">",
-    //   prePageText: "<",
-    //   showTotal: true,
-    //   alwaysShowAllBtns: true,
-    //   onPageChange: function (page, sizePerPage) {
-    //     console.log("page", page);
-    //     console.log("sizePerPage", sizePerPage);
-    //   },
-    //   onSizePerPageChange: function (page, sizePerPage) {
-    //     console.log("page", page);
-    //     console.log("sizePerPage", sizePerPage);
-    //   }
-    // });
-    
 
     const handleChange = (e) => {
 
@@ -87,7 +100,7 @@ export default function Body() {
 
     return (
         <>
-            <div className="mainContainer pt-4 pb-4 d-flex justify-content-between pe-3">
+            <div className="mainContainer pt-4 pb-3 d-flex justify-content-between pe-3">
                 <div aria-label="breadcrumb" className="me-5 ms-3">
                     <ol className="breadcrumb">
                         <li className="breadcrumb-item active" aria-current="page"><a href="#">Müqavilələr</a></li>
@@ -95,7 +108,7 @@ export default function Body() {
                         <li className="breadcrumb-item">Müştəri</li>
                     </ol>
                 </div>
-                <div className="d-flex justify-content-center">
+                <div className="d-flex justify-content-center align-items-start">
                     <div className="input-group">
                         <button
                             className="btn btn-outline-secondary"
@@ -121,7 +134,7 @@ export default function Body() {
                 </div>
             </div>
             <div className="tableContent">
-                <table className="table">
+                {/* <table className="table">
                     <thead className="cell-padding-left-2 ">
                         <tr>
                             <th className=" cell-padding cell-padding-left" scope="col">N.A.S NO:</th>
@@ -148,7 +161,7 @@ export default function Body() {
                                     <td className="cell-padding cell-padding-left-2">{val.cost}</td>
                                     <td className="cell-padding cell-padding-left-2">{val.quantity}</td>
                                     <td className="pt-4">
-                                        <div className="key"><FontAwesomeIcon className="key1"  icon={faKey} /></div>
+                                        <div className="key"><FontAwesomeIcon className="key1" icon={faKey} /></div>
                                     </td>
                                     <td className="cell-padding cell-padding-left"><div className="reason">{val.reason}</div></td>
                                     <td className="cell-padding cell-padding-left pe-4"><div className="status">{val.status}</div></td>
@@ -156,16 +169,28 @@ export default function Body() {
                             ))
                         }
                     </tbody>
+                </table> */}
+
+                <table className="table align-items-center justify-content-center mb-0">
+                    <thead>
+                        <tr>
+                            <th className="cell-padding cell-padding-left" >N.A.S NO:</th>
+                            <th className="cell-padding cell-padding-left" >Reqress NO:</th>
+                            <th className="cell-padding cell-padding-left" >Tarix</th>
+                            <th className="cell-padding cell-padding-left" >Ümumi məbləğ</th>
+                            <th className="cell-padding cell-padding-left" >Miqdar</th>
+                            <th className="cell-padding">E-imza</th>
+                            <th className="cell-padding cell-padding-left-3">Səbəb</th>
+                            <th className="cell-padding cell-padding-left-3">Status</th>
+                        </tr>
+                    </thead>
+                    <ShowData />
                 </table>
-{/*                 
-      <BootstrapTable
-        bootstrap4
-        keyField="id"
-        data={products}
-        columns={columns}
-        // defaultSorted={defaultSorted}
-        pagination={pagination}
-      /> */}
+
+                <div style={{ float: 'right' }}>
+                    <ShowPagination />
+                </div>
+
             </div>
         </>
     )
