c94156c60476f1dcc7d4d741d2e4ba89208c6fc5
Konul304 <memmedovakonul270@gmail.com>
2023-03-09 17:25:56 +0400

    import story images and datas

--- react-design/app/homePage/page.js
+++ react-design/app/homePage/page.js
@@ -19,27 +19,7 @@ import ChartComponent from '../chart';
 import Story from '../story';
 
 export default function HomePage() {
-    const stories = [
-        {
-            type: 'image',
-            url: 'https://images.pexels.com/photos/9470805/pexels-photo-9470805.jpeg?w=300',
-            duration: 5000,
-        },
-        {
-            type: 'image',
-            duration: 6000,
-            url: 'https://images.pexels.com/photos/9733197/pexels-photo-9733197.jpeg?w=300',
-        },
-        {
-            duration: 7000,
-            type: 'image',
-            url: 'https://images.pexels.com/photos/9470805/pexels-photo-9470805.jpeg?w=300',
-        },
-    ];
-
-    const handleClick = () => {
-        return <Stories width="400px" height="600px" stories={stories} />
-    }
+   
     return (
         <>
             <div className='d-flex justify-content-start container'>
--- react-design/app/story.js
+++ react-design/app/story.js
@@ -1,41 +1,76 @@
 import 'stories-react/dist/index.css';
 import Image from 'next/image';
 import story1 from '../public/images/story1.svg'
+import story2 from '../public/images/story2.svg'
+import story3 from '../public/images/story3.svg'
+import story4 from '../public/images/story4.svg'
+import story5 from '../public/images/story5.svg'
+import story6 from '../public/images/story6.svg'
+import story7 from '../public/images/story7.svg'
+import story8 from '../public/images/story8.svg'
+import './css/story.css'
+import Stories from 'stories-react';
+import { useState } from 'react';
+
 
 export default function Story() {
+    const [isShown, setIsShown] = useState(false);
+
+    const stories = [
+        {
+            type: 'image',
+            url: 'https://images.pexels.com/photos/9470805/pexels-photo-9470805.jpeg?w=300',
+            duration: 5000,
+        },
+        {
+            type: 'image',
+            duration: 6000,
+            url: 'https://images.pexels.com/photos/9733197/pexels-photo-9733197.jpeg?w=300',
+        },
+        {
+            duration: 7000,
+            type: 'image',
+            url: 'https://images.pexels.com/photos/9470805/pexels-photo-9470805.jpeg?w=300',
+        },
+    ];
+
+    const handleClick = () => {
+        setIsShown(current => !current);
+    }
     return (
         <>
             <div className='d-flex mt-2 justify-content-between'>
-                <Image 
+                {isShown && <div className="slideshow" ><Stories width="400px" height="600px" stories={stories} /></div>}
+                <Image onClick={handleClick}
                     src={story1}
                     alt="Picture of the author"
                 />
-                 <Image 
-                    src={story1}
+                <Image onClick={handleClick}
+                    src={story2}
                     alt="Picture of the author"
                 />
-                 <Image 
-                    src={story1}
+                <Image onClick={handleClick}
+                    src={story3}
                     alt="Picture of the author"
                 />
-                 <Image 
-                    src={story1}
+                <Image onClick={handleClick}
+                    src={story4}
                     alt="Picture of the author"
                 />
-                 <Image 
-                    src={story1}
+                <Image onClick={handleClick}
+                    src={story5}
                     alt="Picture of the author"
                 />
-                 <Image 
-                    src={story1}
+                <Image onClick={handleClick}
+                    src={story6}
                     alt="Picture of the author"
                 />
-                 <Image 
-                    src={story1}
+                <Image onClick={handleClick}
+                    src={story7}
                     alt="Picture of the author"
                 />
-                 <Image 
-                    src={story1}
+                <Image onClick={handleClick}
+                    src={story8}
                     alt="Picture of the author"
                 />
             </div>
