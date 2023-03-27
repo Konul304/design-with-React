0c42066ab8b4a7882936173d620d422b68665e92
Konul304 <memmedovakonul270@gmail.com>
2023-03-27 12:15:51 +0400

    remove DOM

--- react-design/app/components/story.js
+++ react-design/app/components/story.js
@@ -8,17 +8,14 @@ import pic1 from '../../public/images/pic1.webp'
 
 export default function Story() {
     const [shownIndex, setShownIndex] = useState(-1);
-    const bgRef = useRef(null);
 
     const handleClick = () => {
         setShownIndex(-1);
-        bgRef.current.classList.remove('blur');
     };
     document.body.addEventListener('click', handleClick);
 
     const handlePictureClick = (index) => {
         setShownIndex(index);
-        bgRef.current.classList.add('blur');
     };
 
     const pictures = [
@@ -35,23 +32,26 @@ export default function Story() {
         <>
             <div id="storyFlex" className="d-flex mt-1 justify-content-between">
                 {(shownIndex >= 0) && (
-                    <div className="slideshow" id="story">
-                        <Stories
-                            key={shownIndex}
-                            onAllStoriesEnd={() => {
-                                if (shownIndex === 7) {
-                                    handleClick()
-                                } else {
-                                    setShownIndex((current) => current + 1);
-                                }
-                            }}
-                            defaultDuration={20000}
-                            pauseStoryWhenInActiveWindow={true}
-                            width="400px"
-                            height="600px"
-                            stories={stories[shownIndex]}
-                        />
-                    </div>
+                    <>
+                        <div className='blur' id="bg-blur" ></div>
+                        <div className="slideshow" id="story">
+                            <Stories
+                                key={shownIndex}
+                                onAllStoriesEnd={() => {
+                                    if (shownIndex === 7) {
+                                        handleClick()
+                                    } else {
+                                        setShownIndex((current) => current + 1);
+                                    }
+                                }}
+                                defaultDuration={20000}
+                                pauseStoryWhenInActiveWindow={true}
+                                width="400px"
+                                height="600px"
+                                stories={stories[shownIndex]}
+                            />
+                        </div>
+                    </>
                 )}
                 {pictures.map((picture, i) => (
                     <a className="story" key={i}>
@@ -61,7 +61,6 @@ export default function Story() {
                     </a>
                 ))}
             </div>
-            <div id="bg-blur" ref={bgRef}></div>
         </>
     );
 }
